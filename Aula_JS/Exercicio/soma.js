ba3ac5118431365c947217cda75774ac384cfeb9
Rohy Maia <rohymaia@outlook.com>
2023-06-20 14:53:52 -0300

    acrescentando comentarios

--- Aula_JS/Exercicio/soma.js
+++ Aula_JS/Exercicio/soma.js
@@ -1,11 +1,15 @@
-function soma() {
-    let ler_numero_A = document.getElementById("numeroA").value;
-    let ler_numero_B = document.getElementById("numeroB").value;
+//Criando a função
+function soma() { 
+    let ler_numero_A = document.getElementById("numeroA").value; //Capturando o valor 'A' digitado pelo usuário
+    let ler_numero_B = document.getElementById("numeroB").value; //Capturando o valor 'B' digitado pelo usuário
     
-    let numero_A = parseFloat(ler_numero_A);
+    //convertendo valores que chegam como "Strings" em "Numbers"
+    let numero_A = parseFloat(ler_numero_A); 
     let numero_B = parseFloat(ler_numero_B);
     
+    //Somando os dois valores
     let resultado = numero_A + numero_B;
     
+    //escrevendo para o usuario o resultado
     document.write(`A soma é ${resultado}<br>`);
 }
