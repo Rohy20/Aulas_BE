440ebe00d7514f4e6a80448f5c46df0c57566aae
Rohy Maia <rohymaia@outlook.com>
2023-06-23 22:32:36 -0300

    pequenas alterações

--- Aula_Node/Exercicio/CEP.js
+++ Aula_Node/Exercicio/CEP.js
@@ -1,28 +1,28 @@
 //importando modulos
 const https = require('follow-redirects').https;
-const fs = require('fs');
 
 //Definindo URL
-const BASE_URL = 'example.api.findcep.com';
+const BASE_URL = 'viacep.com.br';
 
 let options = {
   'method': 'GET',
   'hostname': BASE_URL,
-  'path': '/v1/cep/01234000.json',
+  'path': '/ws/01001000/json/',
 };
 
 //Fazendo requisição HTTP
 let req = https.request(options, function (res) {
   let chunks = [];
 
-//tratando eventos de resposta
+  //tratando eventos de resposta
   res.on("data", function (chunk) {
     chunks.push(chunk);
   });
 
   res.on("end", function (chunk) {
     let body = Buffer.concat(chunks);
-    console.log(body.toString());
+    const endereco = JSON.parse(body.toString());
+    console.log("Endereço:", endereco);
   });
 
   res.on("error", function (error) {
@@ -31,4 +31,4 @@ let req = https.request(options, function (res) {
 });
 
 //finalizando requisição
-req.end();
+req.end(); 
