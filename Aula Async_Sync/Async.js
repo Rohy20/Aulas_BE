90cac9b70e02048b081f4ccdc9e9554fbc9e511e
Rohy Maia <rohymaia@outlook.com>
2023-06-27 14:08:52 -0300

    adicionando outros conteudos

---
+++ Aula Async_Sync/Async.js
@@ -0,0 +1,8 @@
+// const fs = require('fs');
+// fs.readFile('dados.txt', (err, data)=> {
+//         if(err) throw err;
+//         console.log(data.toString());
+// });
+
+// console.log('2.Obrigado seja bem-vindo!')
+
---
+++ Aula Async_Sync/Sync.js
@@ -0,0 +1,5 @@
+// const fs = require('fs');
+// const dados = fs.readFileSync('dados.txt', 'utf-8');
+// console.log(dados);
+// console.log('2.Obrigado até breve!')
+
